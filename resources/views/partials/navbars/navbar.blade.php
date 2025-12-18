5f0bdc82fb519c225455a095754050210647cbc2
khairigcor24 <farhan24ti@mahasiswa.pcr.ac.id>
2025-12-18 16:17:27 +0700

    Proyek P3 (18/12/2025)

--- resources/views/partials/navbars/navbar.blade.php
+++
@@ -1,5 +0,0 @@
-@if (auth()->check() && request()->route()->getName() != null)
-    @include('partials.navbars.navs.auth')
-@else
-    @include('partials.navbars.navs.guest')
-@endif
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,37 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PageController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
+
+
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
+
+Route::get('/profile', function () {
+    return view('profile.edit');
+})->name('profile.edit');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/page/{page}', [PageController::class, 'index'])->name('page.index');
+
+Route::resource('user', HomeController::class);
+Route::get('/login', [HomeController::class, 'index'])->name('login');
+
